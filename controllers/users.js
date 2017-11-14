c5ea0f62a3f5bc6a5f802ff9d1b3e001c7b78ec3
ZackDeRose <zack.derose@gmail.com>
2017-11-14 17:47:01 -0500

    eowd 11 14 2017

---
+++ controllers/users.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = function(_){
+  return {
+    SetRouting: function(router){
+      router.get('/', this.indexPage);
+      router.get('/signup', this.getSignUp);
+    },
+    
+    indexPage: function(req, res){
+      res.render('index');
+    },
+    
+    getSignUp: function(req, res){
+      return res.render('signup');
+    }
+    
+  }
+};
